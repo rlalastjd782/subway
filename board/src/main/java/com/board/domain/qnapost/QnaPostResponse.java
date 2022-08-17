05981638f7c2e94c43b2d765d23aa6f4d9b1d649
rlalastjd782 <rlalastjd782@naver.com>
2022-08-17 14:57:58 +0900

    게시판 시간 T 제거

--- board/src/main/java/com/board/domain/qnapost/QnaPostResponse.java
+++ board/src/main/java/com/board/domain/qnapost/QnaPostResponse.java
@@ -2,6 +2,8 @@ package com.board.domain.qnapost;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 @Getter
 public class QnaPostResponse {
@@ -12,7 +14,10 @@ public class QnaPostResponse {
     private String writer;                 // 작성자
     private int viewCnt;                   // 조회 수
     private Boolean noticeYn;              // 공지글 여부
-    private Boolean delYn;                // 삭제 여부
+    private Boolean delYn;   				// 삭제 여부
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd' 'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime writeDate;     // 작성일자
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd' 'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedDate;    // 수정일자
 }
