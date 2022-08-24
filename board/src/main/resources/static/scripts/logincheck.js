e62ba0eba45353ede7df3cfdcfdf31f2f16f33eb
SiriN1234 <hanjongin1@naver.com>
2022-08-24 14:09:09 +0900

    22년 8월 24일 14시 08분 커밋. 로그인/signup버튼 로그인상태면 마이페이지/logout 버튼으로 바뀌게 만듦.

---
+++ board/src/main/resources/static/scripts/logincheck.js
@@ -0,0 +1,54 @@
+// 로그인 되어있는지 체크
+
+let logincheck = "";
+
+$.ajax({
+	type: "post",
+	async: false,
+	url: "/login-check",
+	success:  function (data) {
+		if(data == true){
+			logincheck = true;
+		} else{
+			logincheck = false;
+		}
+	}
+});
+
+const loginbtn = document.getElementById('loginbtn');
+const signupbtn = document.getElementById('signupbtn');
+
+const mypagebtn = document.getElementById('mypagebtn');
+const logoutbtn = document.getElementById('logoutbtn');
+
+if(logincheck){
+	$("#loginbtn").text("마이페이지");
+	$("#signupbtn").text("Logout");
+}
+
+$("#loginbtn").click(function(){
+	
+	if($("#loginbtn").text == "Login"){
+		location.replace('/user/login');
+	} else{
+		location.replace('/user/mypage');
+	}
+
+});
+
+$("#signupbtn").click(function(){
+	
+	if($("#loginbtn").text == "Sign-up"){
+		location.replace('/user/signup');
+	} else{
+		$.ajax({
+				type: "post",
+				async: false,
+				url: "/user/logout",
+				success:  function () {
+					location.replace('/post/main');
+				}
+			})
+	}
+
+});
